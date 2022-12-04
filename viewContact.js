4a0c24c1085a256c1fb49543cfacd31662b76cf5
Kimberly Laughton <kimberlyclaughton@gmail.com>
2022-12-03 19:35:29 -0500

    Added navigation

---
+++ viewContact.js
@@ -0,0 +1,12 @@
+let assignBtn = document.querySelector(".assign")
+let switchBtn = document.querySelector(".switch")
+let notesInput = document.getElementById("noteInput")
+
+assignBtn.addEventListener("click", function(element){
+    element.preventDefault();
+
+});
+switchBtn.addEventListener("click", function(element){
+    element.preventDefault
+    
+});
--- viewContact.php
+++ viewContact.php
@@ -1,1 +1,5 @@
 <?php
+$host = 'localhost';
+$username = 'lab5_user';
+$password = 'password123';
+$dbname = 'world';
