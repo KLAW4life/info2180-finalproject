099455a60dc20f40f3ea7b9d2c93a7df13fca1bd
SC-Dixon <94498106+SC-Dixon@users.noreply.github.com>
2022-12-03 20:53:19 -0500

    Updates schema.sql

--- schema.sql
+++ schema.sql
@@ -1,48 +1,53 @@
-CREATE DATABASE dolphin_crm;
-USE dolphin_crm;
+CREATE DATABASE dolphin_crm2;
+USE dolphin_crm2;
 
-CREATE TABLE Users(
-	id int IDENTITY(1,1),
-	firstname varchar(25),
-	lastname varchar(25),
-	password varchar(20),
-	email varchar(50),
-	role varchar(10),
-	created_at datetime,
+CREATE TABLE `Users`(
+	`id` int NOT NULL AUTO_INCREMENT,
+	`firstname` varchar(25) NOT NULL default '',
+	`lastname` varchar(25) NOT NULL default '',
+	`password` varchar(20) NOT NULL default '',
+	`email` varchar(50) NOT NULL default '',
+	`role` varchar(10) NOT NULL default '',
+	`created_at` datetime,
 
-	PRIMARY KEY(id)
+	PRIMARY KEY(`id`)
 );
 
+ALTER TABLE `Users` AUTO_INCREMENT=1;
 
-CREATE TABLE Contacts(
-	id int IDENTITY(1,1),
-	title varchar(5),
-	firstname varchar(25),
-	lastname varchar(25),
-	email varchar(50),
-	telephone varchar(15),
-	company varchar(50),
-	type varchar(10),
-	assigned_to int,
-	created_by int,
-	created_at datetime,
-	updated_at datetime,
+CREATE TABLE `Contacts`(
+	`id` int NOT NULL AUTO_INCREMENT,
+	`title` varchar(5) NOT NULL default '',
+	`firstname` varchar(25) NOT NULL default '',
+	`lastname` varchar(25) NOT NULL default '',
+	`email` varchar(50) NOT NULL default '',
+	`telephone` varchar(15) NOT NULL default '',
+	`company` varchar(50) NOT NULL default '',
+	`type` varchar(10) NOT NULL default '',
+	`assigned_to` int,
+	`created_by` int,
+	`created_at` datetime,
+	`updated_at` datetime,
 
-	PRIMARY KEY(id),
-	CONSTRAINT fk_Users_Contacts FOREIGN KEY (created_by) REFERENCES Users(id)
+	PRIMARY KEY(`id`),
+	FOREIGN KEY (`created_by`) REFERENCES Users(`id`)
 );
 
-CREATE TABLE Notes(
-	id int IDENTITY(1,1),
-	contact_id int,
-	comment text,
-	created_by int,
-	created_at datetime,
+ALTER TABLE `Contacts` AUTO_INCREMENT=1;
 
-	PRIMARY KEY(id),
-	CONSTRAINT fk_Users_Notes FOREIGN KEY (created_by) REFERENCES Users(id),
-	CONSTRAINT fk_Contacts_Notes FOREIGN KEY (contact_id) REFERENCES Notes(id)
+CREATE TABLE `Notes`(
+	`id` int NOT NULL AUTO_INCREMENT,
+	`contact_id` int NOT NULL,
+	`comment` text NOT NULL default '',
+	`created_by` int,
+	`created_at` datetime,
+
+	PRIMARY KEY(`id`),
+	FOREIGN KEY (`created_by`) REFERENCES Users(`id`),
+	FOREIGN KEY (`contact_id`) REFERENCES Notes(`id`)
 );
 
-INSERT INTO Users (firstname, lastname, password, email, role, created_at) VALUES ('Yannick','Lyn Fatt', '$2y$10$xYiLYzr1b3OlllUvHMZKXu.4jzML9jGsP8eqkQdfyDfMPP1lNQq0a', 'admin@project2.com', 'Admin', getdate());
-SELECT * FROM Users;
+ALTER TABLE `Notes` AUTO_INCREMENT=1;
+
+INSERT INTO Users (`firstname`, `lastname`, `password`, `email`, `role`, `created_at`) VALUES ('Yannick','Lyn Fatt', PASSWORD('password123'), 'admin@project2.com', 'Admin', curdate());
+SELECT * FROM `Users`;
