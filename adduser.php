6faecd4e42009d0ddd195764456c26a4e6aceb47
KLAW4life <kerene789@gmail.com>
2022-12-04 14:26:44 -0500

    Completed Add User functionality

--- adduser.php
+++ adduser.php
@@ -1,68 +1,67 @@
 <?php
-
-// Include config file
-//require_once "adduser_dbconfig.php";
-$fname_raw = $lname_raw = $email_raw = $pword_raw = $role = "";
-
-// Processing form data when form is submitted
-
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-
-    $fname_raw = filt_input($_POST["fname"]);
-    $fname = filter_var($fname_raw, FILTER_SANITIZE_STRING);
-
-    $lname_raw = filt_input($_POST["lname"]);
-    $lname = filter_var($lname_raw, FILTER_SANITIZE_STRING);
-
-    $email_raw = filt_input($_POST["email"]);
-    $email = filter_var($email_raw, FILTER_SANITIZE_EMAIL);
-
-    $pword_raw = filt_input($_POST["pword"]); 
-    $pword = filter_var($pword_raw, FILTER_SANITIZE_STRING);
-    $hashpword = password_hash($pword, PASSWORD_DEFAULT);
-
-  }
-
-function filt_input($data) {
-    $data = trim($data);
-    $data = stripslashes($data);
-    $data = htmlspecialchars($data);
-    return $data;
-  }
-
+session_start();
 ?>
-
-
 
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Add New User</title>
-    <link href="styles.css" type="text/css" rel="stylesheet" />
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@48,400,0,0" />
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script> 
-    <script src="scripts.js" type="text/javascript"></script>
-</head>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Dolphin CRM</title>
+        <link href="home.css" type="text/css" rel="stylesheet" />
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@48,400,0,0" />
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+        <script src="adduser.js" type="text/javascript"></script>
+    </head>
     <body>
-        <div class="wrapper">
+        <header class="headerContainer">
+            <div class="container">
+                <img src="dolphin-svgrepo-com.svg" alt="">
+                <h1>Dolphin CRM</h1>
+            </div>
+        </header>
+        <div class="main-page">
+            <div class="sidebar">
+                <nav>
+                    <li class="side-button">
+                        <span class="material-symbols-rounded">home</span>
+                        <p>Home</p>
+                    </li>
+                    <li class="side-button">
+                        <span class="material-symbols-rounded">person_add</span>
+                        <p>New Contact</p>
+                    </li>
+                    <li class="side-button">
+                        <span class="material-symbols-rounded">group</span>
+                        <p>Users</p>
+                    </li>
+                    <hr>
+                    <li class="side-button">
+                        <span class="material-symbols-rounded">power_settings_new</span>
+                        <p>Logout</p>
+                    </li>
+                </nav>
+            </div>
+            <div class="wrapper">
             <h2>New User</h2>
-            <form id = "adduser" method="post">
+            <form id = "adduser" method="post" action="add_dbuser.php">
                 <div class="form-group">
                     <label>First Name</label>
-                    <input type="text" name="fname" placeholder="Jane" required> 
+                    <input type="text" name="fname" id="fname" placeholder="Jane" required> 
                 </div>    
                 <div class="form-group">
                     <label>Last Name</label>
-                    <input type="text" name="lname" placeholder="Doe" required> 
+                    <input type="text" name="lname" id="lname" placeholder="Doe" required> 
                 </div>
                 <div class="form-group">
                     <label>Email</label>
-                    <input type="text" name="email" placeholder="name@email.com" required> 
+                    <input type="text" name="email" id="email" placeholder="name@email.com" required> 
                 </div> 
                 <div class="form-group">
                     <label>Password</label>
-                    <input type="text" name="pword" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" required 
+                    <input type="text" name="pword" id="pword" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" required 
                     oninvalid="this.setCustomValidity('Password must be at least 8 characters in length and must contain at least one number and one upper case letter.')">
                 </div> 
                 <div class="form-group">
@@ -73,9 +72,15 @@ function filt_input($data) {
                     </select>
                 </div>              
                 <div class="form-group">
-                    <button type="submit">Save</button>
+                    <button type="submit" id="savebtn" name = "savebtn" >Save</button>
                 </div>
             </form>
         </div>    
+        </div>
+        <footer class="footer">
+            <div class="container">
+                <p>Copyright 2022 © Dolphin CRM</p>
+            </div>
+        </footer>
     </body>
 </html>
