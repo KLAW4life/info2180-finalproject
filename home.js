6cdf38791264ec00824f54b8d067a4874d3fa38f
Peter Thelwell <pthelwell55@gmail.com>
2022-12-04 14:23:03 -0500

    Created HTML and CSS for new contact function

--- home.js
+++ home.js
@@ -17,6 +17,44 @@ function allUsers(){
     })
 }
 
+function salesLeads(){
+    document.getElementById("sales-lead").addEventListener("click", function(event){
+        event.preventDefault();
+
+        var filterSpace = document.getElementById("contact-table");
+        var httpReq = new XMLHttpRequest();
+        var url = "http://localhost/info2180-finalproject/dash.php";
+
+        httpReq.onreadystatechange = function(){
+            if(this.readyState == XMLHttpRequest.DONE && this.status == 200){
+                filterSpace.innerHTML = this.responseText;
+            }
+        }
+
+        httpReq.open("GET", url);
+        httpReq.send();
+    })
+}
+
+function supports(){
+    document.getElementById("support").addEventListener("click", function(event){
+        event.preventDefault();
+
+        var filterSpace = document.getElementById("contact-table");
+        var httpReq = new XMLHttpRequest();
+        var url = "http://localhost/info2180-finalproject/dash.php";
+
+        httpReq.onreadystatechange = function(){
+            if(this.readyState == XMLHttpRequest.DONE && this.status == 200){
+                filterSpace.innerHTML = this.responseText;
+            }
+        }
+
+        httpReq.open("GET", url);
+        httpReq.send();
+    })
+}
+
 function viewContact(){
     document.getElementById("view").addEventListener("click", function(event){
         event.preventDefault();
@@ -24,7 +62,22 @@ function viewContact(){
     })
 }
 
+function newContact(){
+    document.getElementById("contact-btn").addEventListener("click", function(event){
+        event.preventDefault();
+        window.location = "newcontact.html";
+    })
+
+    document.getElementById("new-contact-btn").addEventListener("click", function(event){
+        event.preventDefault();
+        window.location = "newcontact.html";
+    })
+}
+
 window.addEventListener("DOMContentLoaded", function(){
     allUsers();
+    salesLeads();
+    supports();
     viewContact();
+    newContact();
 })
